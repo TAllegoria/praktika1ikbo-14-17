2e6a898f76eec34f7370b375d0e67668c646ffe7
TAllegoria <32561238+TAllegoria@users.noreply.github.com>
2018-09-17 09:48:58 +0300

    Add files via upload

---
+++ practice 2/TestAuthor.java
@@ -0,0 +1,12 @@
+package author;
+
+public class TestAuthor {
+
+	public static void main(String[] args) {
+		Author a1 = new Author("Tan Ah Teck", "ahTeck@somewhere.com", 'm');
+		Author a2 = new Author("Sue Grant", "suGrant@somewhere.com", 'm');
+		System.out.println(a1);
+		System.out.println(a2);
+	}
+
+}
